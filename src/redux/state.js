82276f15a9dc58a89597efe19c42ed4d90efea06
Roman Lukachyk <petrovroman26@icloud.com>
2022-11-02 15:05:02 +0200

    33 lesson add rerender for HTml page

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+import { rerenderEntireTree } from "../render";
+
 let state = {
   profilePage: {
     postData: [
@@ -30,6 +32,7 @@ export let addPost = (postMessage) => {
     like: 0,
   };
   state.profilePage.postData.push(newPostData);
+  rerenderEntireTree(state);
 };
 
 export default state;
