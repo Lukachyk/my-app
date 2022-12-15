f0961dc432fcd6056568175ca5d590aab5a1030d
Roman Lukachyk <petrovroman26@icloud.com>
2022-12-15 14:00:28 +0200

    75 login form redux-form

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 import dialogsReducer from "./dialogs-reduce";
 import profileReducer from "./profile-reduce";
@@ -13,6 +14,7 @@ let reducers = combineReducers({
   sidebar: sidebarReducer,
   usersPage: usersReducer,
   auth: authReducer,
+  form: formReducer,
 });
 
 let store = legacy_createStore(reducers, applyMiddleware(thunkMiddleware));
